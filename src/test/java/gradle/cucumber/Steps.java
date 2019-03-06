528b37a2a8a0f014e67c40633cc2326aa4d382ad
ahmadowais123 <owais.khan2@mail.mcgill.ca>
2019-03-05 23:55:40 -0500

    Setup gmail API

--- src/test/java/gradle/cucumber/Steps.java
+++ src/test/java/gradle/cucumber/Steps.java
@@ -1,5 +1,22 @@
 package gradle.cucumber;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -15,11 +32,26 @@ public class Steps {
 
     public static WebDriver chrome;
     public static WebDriverWait wait;
-    public static final String CHROME_DRIVER_PATH = "Driver/chromedriver.exe";
+    public static Gmail service;
+    public List<String> SCOPES;
+    public final String CHROME_DRIVER_PATH = "Driver/chromedriver.exe";
+    private final String APPLICATION_NAME = "Gmail API Java Quickstart";
+    private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private final String TOKENS_DIRECTORY_PATH = "tokens";
+    private final String CREDENTIALS_FILE_PATH = "src/test/resources/credentials.json";
 
     @Given("^I have a gmail account$")
     public void setupSeleniumDriverForTests() throws Exception {
         setupSeleniumDriver();
+        SCOPES = new ArrayList<>();
+        SCOPES.add(GmailScopes.GMAIL_COMPOSE);
+        SCOPES.add(GmailScopes.GMAIL_SEND);
+
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
     }
 
     @When("I login with username {string} and password {string}")
@@ -74,5 +106,27 @@ public class Steps {
             chrome = new ChromeDriver();
             wait = new WebDriverWait(chrome, 30);
         }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        File file = new File(CREDENTIALS_FILE_PATH);
+        InputStream in = new FileInputStream(file);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 }
--- src/test/resources/Mytest.feature
+++ src/test/resources/Mytest.feature
@@ -4,9 +4,9 @@ Feature: Reset functionality on login page of Application
   Scenario: Send an email with image attachment
 
     Given I have a gmail account
-    When I login with username "username" and password "password"
-    And I click on the compose button
-    And I send the email to "owais.khan2@mail.mcgill.ca" with subject "Test Email"
-    And I attach an image
-    And I click the submit button
-    Then The email should be sent successfully
+#    When I login with username "username" and password "password"
+#    And I click on the compose button
+#    And I send the email to "owais.khan2@mail.mcgill.ca" with subject "Test Email"
+#    And I attach an image
+#    And I click the submit button
+#    Then The email should be sent successfully

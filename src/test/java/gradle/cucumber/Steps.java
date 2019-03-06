911df0b44181be5b369dfb7fa001852cf03bc249
ahmadowais123 <owais.khan2@mail.mcgill.ca>
2019-03-05 21:52:38 -0500

    Autoit script to attach image

--- src/test/java/gradle/cucumber/Steps.java
+++ src/test/java/gradle/cucumber/Steps.java
@@ -1,5 +1,7 @@
 package gradle.cucumber;
 
+import com.gargoylesoftware.htmlunit.WebWindow;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -12,33 +14,67 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Steps {
 
-    public static WebDriver webDriver;
+    public static WebDriver chrome;
+    public static WebDriverWait wait;
     public static final String CHROME_DRIVER_PATH = "Driver/chromedriver.exe";
 
-    @Given("^I open chrome and navigate to gmail$")
-    public void openChromeAndGoToGmail() throws Exception {
-
+    @Given("^I have a gmail account$")
+    public void setupSeleniumDriverForTests() throws Exception {
         setupSeleniumDriver();
+    }
 
-        webDriver.get("https://accounts.google.com/signin");
-        Thread.sleep(5000);  // Let the user actually see something!
-//        WebDriverWait wait = new WebDriverWait(webDriver, 10);
-//        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("Xb9hP")));
-        WebElement email = webDriver.findElement(By.id("identifierId"));
-        email.click();
-        email.sendKeys("ahmadowais123");
-        //WebElement searchBox = webDriver.findElement(By.name("q"));
-        //searchBox.sendKeys("ChromeDriver");
-        //searchBox.submit();
-        //Thread.sleep(5000);  // Let the user actually see something!
-        //webDriver.quit();
+    @When("I login with username {string} and password {string}")
+    public void gmailLoginUsernamePassword(String username, String password) {
+
+        chrome.get("https://mail.google.com");
+        WebElement emailElement = chrome.findElement(By.id("identifierId"));
+        emailElement.click();
+        emailElement.sendKeys(username);
+        chrome.findElement(By.id("identifierNext")).click();
+        WebElement passwordElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("password")));
+        passwordElement.sendKeys(password);
+        chrome.findElement(By.id("passwordNext")).click();
+
+    }
+
+    @And("^I click on the compose button$")
+    public void clickComposeButton() {
+        WebElement composeButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@role='button' and text()='Compose']")));
+        composeButton.click();
+    }
+
+    @And("I send the email to {string} with subject {string}")
+    public void setEmailAddressAndSubject(String emailAddress, String subject) {
+        WebElement recipientAddressElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//textarea[@name='to' and @aria-label='To']")));
+        recipientAddressElement.sendKeys(emailAddress);
+
+        WebElement subjectElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='subjectbox' and @aria-label='Subject']")));
+        subjectElement.sendKeys(subject);
+
+        WebElement bodyElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@aria-label='Message Body']")));
+        bodyElement.sendKeys("Email sent by selenium");
+    }
+
+    @And("^I attach an image$")
+    public void attachImage() throws Exception {
+        WebElement attachFileElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@command='Files' and @aria-label='Attach files']")));
+        attachFileElement.click();
+        Runtime.getRuntime().exec("Autoit/attach.exe");
+        Thread.sleep(5000);
+    }
+
+    @And("^I click the submit button$")
+    public void sendEmail() {
+        WebElement sendButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@role='button' and @aria-label='Send \u202A(Ctrl-Enter)\u202C']")));
+        sendButton.click();
     }
 
     private void setupSeleniumDriver() {
-        if(webDriver == null) {
+        if(chrome == null) {
             System.out.println("Setting up chrome driver");
             System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
-            webDriver = new ChromeDriver();
+            chrome = new ChromeDriver();
+            wait = new WebDriverWait(chrome, 30);
         }
     }
 }
